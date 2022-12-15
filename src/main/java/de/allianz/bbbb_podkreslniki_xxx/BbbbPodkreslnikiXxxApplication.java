3bc553a57b22ea02f012396101da406c3b1c20df
jarro06 <102299007+jarro06@users.noreply.github.com>
2022-12-15 10:53:35 +0100

    Create maven project

---
+++ src/main/java/de/allianz/bbbb_podkreslniki_xxx/BbbbPodkreslnikiXxxApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.bbbb_podkreslniki_xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbbbPodkreslnikiXxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbbbPodkreslnikiXxxApplication.class, args);
+	}
+
+}
